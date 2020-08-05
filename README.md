98b88cd7947e09f59796a8d97fac248028bce1bd
Andy Paine <andy.paine@engineerbetter.com>
2020-08-05 10:17:15 +0100

    Add Jaeger's HotROD as a demonstration
    HotROD requires 4 microservices talking locally to one another. It
    expects to talk on 0.0.0.0 to all other microservices so is hard to
    distribute. By running a Consul proxy next to all apps, we can pretend
    things are local whilst getting mTLS, metrics etc. for all microservice
    communications

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Bring your own service mesh to CF (using Consul)
 
-Requires `consul` and `envoy` (1.14.2) binaries for linux to be present in `bin/` directory when doing `cf push`
+Requires `consul`,`envoy` (1.14.2) and `hotrod` binaries for linux to be present in `bin/` directory when doing `cf push`
--- network-policies.rb
+++ network-policies.rb
@@ -8,9 +8,9 @@ apps = manifest['applications'].map do |app|
   app['name']
 end
 apps.each do |app|
-  system "cf add-network-policy #{app} #{app} --port 8300-8302 --protocol tcp"
+  system "cf add-network-policy #{app} #{app} --port 8000-9000 --protocol tcp"
 end
 apps.permutation(2) do |perm|
-  system "cf add-network-policy #{perm[0]} #{perm[1]} --port 8300-8302 --protocol tcp"
+  system "cf add-network-policy #{perm[0]} #{perm[1]} --port 8000-9000 --protocol tcp"
   system "cf add-network-policy #{perm[0]} #{perm[1]} --port 8301-8302 --protocol udp"
 end
